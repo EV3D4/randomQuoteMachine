90299e118478c1dcfe7b059048a0d20011cdb6f6
kalpitp <kalpit.parikh@outlook.com>
2019-09-07 15:00:51 -0400

    updated quote

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ function caseInSwitch(val) {
      caseString = "I become what I see in myself. All that thought suggests to me, I can do; all that thought reveals in me, I can become. This should be man's unshakeable faith in himself, because God dwells in him. <br><br> -Sri Aurobindo";
      break;
    case 1:
-     caseString = "The Age of adventure is over. Even if we go to the seventh galaxy, we will go there helmeted and mechanized, and we will find ourselves exactly as we are: children in the face of death, living beings who are no too sure ho theylive or why, nor where they are going. <br><br> -Sri Aurobindo";
+     caseString = "The age of adventures is over. Even if we reach the seventh galaxy, we will go there helmeted and mechanized, and it will not change a thing for us; we will find ourselves exactly as we are now: helpless children in the face of death, living beings who are not too sure how they live, why they are alive, or where they are going. <br><br> -Sri Aurobindo";
      break;
    case 2:
      caseString = "The heavens beyond are great and wonderful, but greater and more wonderful are the heavens within you. It is these Edens that await the divine worker. <br><br> -Sri Aurobindo";
