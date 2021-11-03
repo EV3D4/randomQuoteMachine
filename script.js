e389f355d1ea18b31b9fb25ed0368e00d0b9862b
Kalpit Parikh <liber.arium@icloud.com>
2021-11-03 13:26:43 -0500

    Update script.js

--- script.js
+++ script.js
@@ -39,25 +39,25 @@ function caseInSwitch(val) {
  var caseString = "";
  switch (val) {
    case 0:
-     caseString = "I become what I see in myself. All that thought suggests to me, I can do; all that thought reveals in me, I can become. This should be man's unshakeable faith in himself, because God dwells in him. <br><br> -Satprem";
+     caseString = "I become what I see in myself. All that thought suggests to me, I can do; all that thought reveals in me, I can become. This should be man's unshakeable faith in himself, because God dwells in him. <br><br> Sri Aurobindo, <br> Thoughts and Glimpses";
      break;
    case 1:
-     caseString = "The age of adventures is over. Even if we reach the seventh galaxy, we will go there helmeted and mechanized, and it will not change a thing for us; we will find ourselves exactly as we are now: helpless children in the face of death, living beings who are not too sure how they live, why they are alive, or where they are going. <br><br> -Satprem";
+     caseString = "The soul attracted leaned to the Abyss: <br> It longed for the adventure of Ignorance...<br><br> Sri Aurobindo, <br> Savitri" 
      break;
    case 2:
-     caseString = "The heavens beyond are great and wonderful, but greater and more wonderful are the heavens within you. It is these Edens that await the divine worker. <br><br> -Satprem";
+     caseString = "The heavens beyond are great and wonderful, but greater and more wonderful are the heavens within you. It is these Edens that await the divine worker. <br><br> Sri Aurobindo, <br> Hour of God";
      break;
    case 3:
-     caseString = "Our actual enemy is not any force exterior to ourselves, but our own crying weaknesses, our cowardice, our selfishness, our hypocrisy, our purblind sentimentalism. <br><br> -Satprem";
+     caseString = "Our actual enemy is not any force exterior to ourselves, but our own crying weaknesses, our cowardice, our selfishness, our hypocrisy, our purblind sentimentalism. <br><br> Sri Aurobindo, <br> New Lamps for Old";
      break;
    case 4:
-     caseString = "For what the Spirit sees becomes a truth. <br> And what the soul imagines is made a world. <br><br> -Satprem";
+     caseString = "For what the Spirit sees becomes a truth. <br> And what the soul imagines is made a world. <br><br> Sri Aurobindo, <br>Savitri";
      break;
    case 5:
-     caseString = "For Truth and knowldge are an idle gleam, <br> If Knowldge brings not power to change the world. <br><br> -Satprem";
+     caseString = "For Truth and knowldge are an idle gleam, <br> If Knowldge brings not power to change the world. <br><br> Sri Aurobindo, <br>Savitri";
      break;
    default:
-     caseString = "I never admitted a truth in the mind without simultaneously keeping it open to the contrary of it...And the first result was the prestige of the intellect was gone. <br> <br>-Satprem";
+     caseString = "I never admitted a truth in the mind without simultaneously keeping it open to the contrary of it <br> And the first result was the prestige of the intellect was gone. <br> <br> Sri Aurobindo, <br>Evening Talks ";
      break;
  }
  return caseString;
